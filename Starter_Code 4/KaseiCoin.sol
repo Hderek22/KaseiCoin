c288ea7ffbd69688d097a4f2df1b3d009518cc0f
Derek Hall <94722790+Hderek22@users.noreply.github.com>
2022-05-01 21:33:52 -0400

    initial file upload

---
+++ Starter_Code 4/KaseiCoin.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+//  Import the following contracts from the OpenZeppelin library:
+//    * `ERC20`
+//    * `ERC20Detailed`
+//    * `ERC20Mintable`
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+
+// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+
+
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint initialSupply
+    )   
+        ERC20Detailed (name, symbol, 18) public{
+
+        }
+
+
+}
